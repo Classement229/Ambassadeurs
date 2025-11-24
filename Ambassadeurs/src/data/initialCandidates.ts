45fed3cf2d926500bc2d8bf027eda6f540602cc8
Classement229 <thiagaagbessi@gmail.com>
2025-11-24 11:50:28 +0100

    Delete Ambassadeurs directory

--- Ambassadeurs/src/data/initialCandidates.ts
+++
@@ -1,7 +0,0 @@
-import type { Candidate } from '../types';
-
-export const initialCandidates: Candidate[] = Array.from({ length: 50 }, (_, i) => ({
-  id: i + 1,
-  name: `Candidat ${i + 1}`,
-  votes: Math.floor(Math.random() * 500) + 50,
-}));
